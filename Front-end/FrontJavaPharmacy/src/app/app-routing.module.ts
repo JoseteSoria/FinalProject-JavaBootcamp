8bc273af74644e97d7bd88ab259b58464e1b8978
JoseSoria <josete.soria@gmail.com>
2020-07-20 20:46:28 +0200

    Check conection and cors

--- Front-end/FrontJavaPharmacy/src/app/app-routing.module.ts
+++ Front-end/FrontJavaPharmacy/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MedicineListComponent } from './components/medicine-list/medicine-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'medicines', component: MedicineListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Front-end/FrontJavaPharmacy/src/app/app.module.ts
+++ Front-end/FrontJavaPharmacy/src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { MatTableModule } from '@angular/material/table';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MedicineListComponent } from './components/medicine-list/medicine-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MedicineListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
